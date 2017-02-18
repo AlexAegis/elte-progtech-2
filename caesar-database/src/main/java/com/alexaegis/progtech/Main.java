e75437c05260cc9682cea73e32890994dce7c2a0
AlexAegis <alexaegis@gmail.com>
2017-02-18 19:01:42 +0100

    init

---
+++ caesar-database/src/main/java/com/alexaegis/progtech/Main.java
@@ -0,0 +1,9 @@
+package com.alexaegis.progtech;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!");
+    }
+
+}
---
+++ lesson01/src/test/java/com/github/alexaegis/MainTest.java
@@ -0,0 +1,14 @@
+package com.github.alexaegis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
