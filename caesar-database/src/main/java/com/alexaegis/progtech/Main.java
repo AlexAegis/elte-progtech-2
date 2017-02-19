1dba86f5d05196260b2b51b0ea9d8cfa0d047da0
AlexAegis <alexaegis@gmail.com>
2017-02-19 13:28:54 +0100

    sql connection done, program recieves properties from arguments

--- caesar-database/src/main/java/com/alexaegis/progtech/Main.java
+++ caesar-database/src/main/java/com/alexaegis/progtech/Main.java
@@ -1,15 +1,69 @@
 package com.alexaegis.progtech;
 
 import com.alexaegis.progtech.database.Connector;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.properties.EncryptableProperties;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    private static final String dbprops = "database.properties";
+    private static final String sshprops = "ssh.properties";
+    public static final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+    public static Properties dbProperties = new EncryptableProperties(encryptor);
+    public static Properties sshProperties = new EncryptableProperties(encryptor);
+
+    @Parameter(names = {"--dbdriver", "-dbd"}) static String dbdriver;
+    @Parameter(names = {"--jdbcdriver", "-jdbc"}) static String jdbcdriver;
+    @Parameter(names = {"--dbprotocol", "-dbprot"}) static String dbprotocol;
+    @Parameter(names = {"--dbrhost", "-dbrh", "--dbhost", "-dbh"}) static String dbrhost;
+    @Parameter(names = {"--dbremoteport", "--dbrport", "-dbrport", "-dbrp"}) static String dbrport;
+    @Parameter(names = {"--dblhost", "-dblh"}) static String dblhost;
+    @Parameter(names = {"--dblocalport", "--dblport", "-dblport", "-dblp"}) static String dblport;
+    @Parameter(names = {"--dbschema", "-dbsch", "-dbs", "--dbname", "-dbn"}) static String dbschema;
+    @Parameter(names = {"--dbusername", "-dbun", "--dbuser", "-dbu"}) static String dbusername;
+    @Parameter(names = {"--dbpassword", "-dbpw"}) static String dbpassword;
+
+    @Parameter(names = {"--sshhost", "-sshh"}) static String sshhost;
+    @Parameter(names = {"--sshport", "-sshp"}) static String sshport;
+    @Parameter(names = {"--sshusername", "-sshun", "--sshuser", "-sshu"}) static String sshusername;
+    @Parameter(names = {"--sshpassword", "-ssnpw"}) static String sshpassword;
+
+    public static void main(String[] args) {
+        new JCommander(new Main(), args);
+        encryptor.setPassword(Integer.toString(new Random().nextInt(Integer.MAX_VALUE)));
+        updateProperties();
+
         Connector connector = new Connector();
-        connector.connect();
-        System.out.println(connector.toString());
+    }
+
+    private static void updateProperties() {
+        try {
+            dbProperties.load(Main.class.getClassLoader().getResourceAsStream(dbprops));
+            sshProperties.load(Main.class.getClassLoader().getResourceAsStream(sshprops));
+
+            if(dbdriver != null) dbProperties.setProperty("driver", dbdriver);
+            if(jdbcdriver != null) dbProperties.setProperty("jdbcdriver", jdbcdriver);
+            if(dbprotocol != null) dbProperties.setProperty("protocol", dbprotocol);
+            if(dbrhost != null) dbProperties.setProperty("rhost", dbrhost);
+            if(dbrport != null) dbProperties.setProperty("rport", dbrport);
+            if(dblhost != null) dbProperties.setProperty("lhost ", dblhost);
+            if(dblport != null) dbProperties.setProperty("lport", dblport);
+            if(dbschema != null) dbProperties.setProperty("schema", dbschema);
+            if(dbusername != null) dbProperties.setProperty("username", dbusername);
+            if(dbpassword != null) dbProperties.setProperty("password", encryptor.encrypt(dbpassword));
+
+            if(sshhost != null) sshProperties.setProperty("host", sshhost);
+            if(sshport != null) sshProperties.setProperty("port", sshport);
+            if(sshusername != null) sshProperties.setProperty("username", sshusername);
+            if(sshpassword != null) sshProperties.setProperty("password", encryptor.encrypt(sshpassword));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- caesar-database/src/main/java/com/alexaegis/progtech/database/Connector.java
+++ caesar-database/src/main/java/com/alexaegis/progtech/database/Connector.java
@@ -2,75 +2,51 @@ package com.alexaegis.progtech.database;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 import java.io.IOException;
 import java.sql.*;
-import java.util.Properties;
 import java.util.logging.Logger;
+
+import static com.alexaegis.progtech.Main.dbProperties;
+import static com.alexaegis.progtech.Main.encryptor;
+import static com.alexaegis.progtech.Main.sshProperties;
 
 public class Connector {
 
-    private final String DBPROPS = "database.properties";
-    private final String SSHPROPS = "ssh.properties";
-    private final Properties dbProperties;
-    private final Properties sshProperties;
-    private final String jdbcDriver;
-    private final String sshHost;
-    private final int sshPort;
-    private final String sshUsername;
-    private final String sshPassword;
-    private final String dbDriver;
-    private final String dbProtocol;
-    private final String dbHost;
-    private final String dbLocalHost;
-    private final int dbPort;
-    private final int dbLocalPort;
-    private final String dbName;
-    private final String dbUsername;
-    private final String dbPassword;
-    private final String dbUrl;
-    private final String dbLocalUrl;
+    private final String dbDriver = dbProperties.getProperty("driver");
+    private final String jdbcDriver = dbProperties.getProperty("jdbcdriver");
+    private final String dbProtocol = dbProperties.getProperty("protocol");
+    private final String dbRemoteHost = dbProperties.getProperty("rhost");
+    private final int dbRemotePort = Integer.parseInt(dbProperties.getProperty("rport"));
+    private final String dbLocalHost = dbProperties.getProperty("lhost");
+    private final int dbLocalPort = Integer.parseInt(dbProperties.getProperty("lport"));
+    private final String dbSchema = dbProperties.getProperty("schema");
+    private final String dbUsername = dbProperties.getProperty("username");
+    private final String dbPassword = dbProperties.getProperty("password");
+    private final String sshHost = sshProperties.getProperty("host");
+    private final int sshPort = Integer.parseInt(sshProperties.getProperty("port"));
+    private final String sshUsername = sshProperties.getProperty("username");
+    private final String sshPassword = sshProperties.getProperty("password");
 
-    {
-        dbProperties = new Properties();
-        sshProperties = new Properties();
-        dbProperties.load(Connector.class.getClassLoader().getResourceAsStream(DBPROPS));
-        sshProperties.load(Connector.class.getClassLoader().getResourceAsStream(SSHPROPS));
-        dbDriver = dbProperties.getProperty("driver");
-        dbProtocol = dbProperties.getProperty("protocol");
-        dbHost = dbProperties.getProperty("host");
-        dbLocalHost = dbProperties.getProperty("localhost");
-        dbPort = Integer.parseInt(dbProperties.getProperty("port"));
-        dbLocalPort = Integer.parseInt(dbProperties.getProperty("localport"));
-        dbName = dbProperties.getProperty("databasename");
-        dbUsername = dbProperties.getProperty("username");
-        dbPassword = dbProperties.getProperty("password"); // TODO prompt me
-        jdbcDriver = dbProperties.getProperty("jdbcdriver");
-        dbUrl = dbProtocol + ":" + dbDriver + "://" + dbHost + ":" + dbPort + "/" + dbName;
-        dbLocalUrl = dbProtocol + ":" + dbDriver + "://" + dbLocalHost + ":" + dbLocalPort + "/" + dbName;
-        sshHost = sshProperties.getProperty("host");
-        sshPort = Integer.parseInt(sshProperties.getProperty("port"));
-        sshUsername = sshProperties.getProperty("username");
-        sshPassword = sshProperties.getProperty("password"); // TODO prompt me
-    }
+    private final String dbRemoteUrl = dbProtocol + ":" + dbDriver + "://" + dbRemoteHost + ":" + dbRemotePort + "/" + dbSchema;
+    private final String dbLocalUrl = dbProtocol + ":" + dbDriver + "://" + dbLocalHost + ":" + dbLocalPort + "/" + dbSchema;
 
-    private JSch sshConnection = new JSch();
+    private JSch sch = new JSch();
     private Session session;
     private Logger logger = Logger.getLogger(Connector.class.getName());
 
-    public Connector() throws IOException, SQLException {
+    public Connector() {
         try {
-            session = sshConnection.getSession(sshUsername, sshHost, sshPort);
-            session.setPassword(sshPassword);
+            session = sch.getSession(sshUsername, sshHost, sshPort);
+            session.setPassword(encryptor.decrypt(sshPassword));
             session.setConfig("StrictHostKeyChecking", "no");
             session.connect();
             logger.info("SSH Connected to " + session.getHost());
-            session.setPortForwardingL(dbLocalPort, dbLocalHost, dbPort);
+            session.setPortForwardingL(dbLocalPort, dbRemoteHost, dbRemotePort);
 
-            Class.forName(jdbcDriver);
-
-            Connection conn = DriverManager.getConnection(dbLocalUrl, dbUsername, dbPassword);
-
+            Class.forName(jdbcDriver).newInstance();
+            Connection conn = DriverManager.getConnection(dbLocalUrl, dbUsername, encryptor.decrypt(dbPassword));
 
             // our SQL SELECT query.
             // if you only need a few columns, specify them by name instead of using "*"
@@ -78,18 +54,20 @@ public class Connector {
 
             // create the java statement
             Statement st = conn.createStatement();
-
             // execute the query, and get a java resultset
             ResultSet rs = st.executeQuery(query);
-
-            // iterate through the java resultset
-            while (rs.next())
-            {
-                String firstName = rs.getString("Name");
-
-                // print the results
-                System.out.format("%s \n", firstName);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
             }
+
+            conn.close();
             rs.close();
             st.close();
         }
